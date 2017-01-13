53c79186f581529352cc6966acfc575b80f65105
Nguyen Vuong Vu <vuongvu@cubevn.local>
2017-01-13 11:40:53 +0700

    a

--- app/webroot/js/customer.js
+++ app/webroot/js/customer.js
@@ -10,25 +10,24 @@ $(document).ready(function() {
 
 	var dataTable;
 
-	var newRow = '<tr>' + 
-			'<td></td>' + '<td></td>' + '<td></td>' + '<td></td>' + '<td></td>' + '<td></td>' +
-			'<td></td>' + '<td></td>' + '<td></td>' + '<td></td>' + '<td></td>' + '<td></td>' +
-			'<td></td>' + '<td></td>' + '<td></td>' + '<td></td>' + '<td></td>' + '<td></td>' + '<td></td>' +
-			'</tr>';
-
 	function initDataTable() {
 		dataTable = $('#searchResult').DataTable(options);
 	}
 
 	function attachClickHandler() {
 		$('.btn-addrow').click(function() {
-			var rowIndex = parseInt($(this).attr('id'));
+			var index = parseInt($(this).attr('id'));
+			$(this).attr('index', index);
 		});
 	}
 
 	$('.btn-addrow').click(function() {
 		var index = $(this).attr('index');
-
+		var newRow = '<tr>' + 
+			'<td></td>' + '<td></td>' + '<td></td>' + '<td></td>' + '<td></td>' + '<td></td>' +
+			'<td></td>' + '<td></td>' + '<td></td>' + '<td></td>' + '<td></td>' + '<td></td>' +
+			'<td></td>' + '<td></td>' + '<td></td>' + '<td></td>' + '<td></td>' + '<td></td>' + '<td></td>' +
+			'</tr>';
 		dataTable.destroy();
 		$("#searchResult tbody tr").eq(index).after(newRow);    
 	    initDataTable();
